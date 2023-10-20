de87273e714f3cf81c660f4231c04de5a4c742f6
xswle <361039005@qq.com>
2023-10-20 23:26:30 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    const a = 10
+    console.log(a)
+  }
+}
+</script>
